c384dfbc94d52c01dc203cff026e046151d5b4fb
Christian Reed <chreed87@gmail.com>
2025-03-07 11:32:46 +0100

    made a small js function that adds showpassword to a password field

--- showHidePassword/js/script.js
+++ showHidePassword/js/script.js
@@ -1,12 +1,16 @@
-const showPassword = document.getElementById('showPass');
+const checkBox = document.getElementById('check-box');
 const passwordField = document.getElementById('passwordBox');
 
-showPassword.checked = false;
-
-function letsSeePassword() {
-	if ((showPassword.checked = true)) {
+//function that checks if the checkbox is "checked" and changes
+//the the password input field to a text field if true
+function showPassword() {
+	if (checkBox.checked == true) {
 		passwordField.type = 'text';
-	} 
+	} else {
+		passwordField.type = 'password';
+	}
 }
 
-showPassword.addEventListener('click', letsSeePassword);
+//eventListener for if there is a change in the checkbox
+// i.e it checked then call function showPassword
+checkBox.addEventListener('change', showPassword);
